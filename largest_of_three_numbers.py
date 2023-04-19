72379cc73558d7bb643d2a2b0f70d5d497fe6fc8
22ds30000048 <131288367+22ds30000048@users.noreply.github.com>
2023-04-19 22:46:01 +0530

    Update largest_of_three_numbers.py

--- largest_of_three_numbers.py
+++ largest_of_three_numbers.py
@@ -27,7 +27,7 @@ num1 = st.number_input("Enter first number: ")
 num2 = st.number_input("Enter second number: ")
 num3 = st.number_input("Enter third number: ")
 
-largest_num(num1,num2,num3)
+
     if (num1 >= num2) and (num1 >= num3):
            largest = num1
     elif (num2 >= num1) and (num2 >= num3):
@@ -36,4 +36,4 @@ largest_num(num1,num2,num3)
            largest = num3}
     
 
-st.write("The largest number is", largest_num(n1,n2,n3)
+st.write("The largest number is", largest)
